a7f90950a2243cc9d8dcb5cb12b6bbe95a3c45c7
GUSTAVO JANEBRO DA CUNHA <66034440+Dilacrius@users.noreply.github.com>
2022-02-06 09:46:28 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,37 @@
 
+class Calculadora {
+  //Campos
+  int valor_1;
+  int valor_2;
+  
+  //Construtores
+  Calculadora(
+    this.valor_1,
+    this.valor_2,
+  );
+  
+  //Metodos
+  somar(){
+    print('Valor da soma $valor_1+$valor_2: ${valor_1+valor_2}');
+  }
+  
+  subtrair(){
+    print('Valor da subtração $valor_1-$valor_2: ${valor_1-valor_2}');
+  }
+  
+  multiplicar(){
+    print('Valor da multiplicação $valor_1*$valor_2: ${valor_1*valor_2}');
+  }
+  
+  dividir(){
+    print('Valor da divisão $valor_1/$valor_2: ${valor_1/valor_2}');
+  }
+}
+
+void main() {
+  Calculadora valor = Calculadora(5,3);
+  valor.somar();
+  valor.subtrair();
+  valor.multiplicar();
+  valor.dividir();
+}
